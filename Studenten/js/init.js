89b4b22a5f461b92416c45d37d7ac6cf36202a92
Misha <michakiber1@gmail.com>
2016-02-10 21:15:39 +0100

    - Today's changes on documents - Latest version website design

--- Studenten/js/init.js
+++ Studenten/js/init.js
@@ -14,8 +14,16 @@
     }
 
     // Plugin initialization
-	$('.dropdown-button').dropdown({hover: false});
-    $('.button-collapse').sideNav({'edge': 'left'});
+	$('.more').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on click
+      alignment: 'left', // Aligns dropdown to left or right edge (works with constrain_width)
+      gutter: 0, // Spacing from edge
+      belowOrigin: false // Displays dropdown below the button
+    }
+  );
 	$('.materialboxed').materialbox();
 	$("#studentsTable").tablesorter();
 	$("#usersTable").tablesorter(); 
