6ab899fdeb85201d770c53736325f77ab1695ca5
Mikhail Lashkov <michakiber1@gmail.com>
2016-02-20 01:33:36 +0100

    - Fixed DatePicker, now it's possible to pick a year. - Made "see more" responsive. - Added icons when sorting in table. - Added text logo to header. - Added possibility to see your profile and your account rights to header.

--- Studenten/js/init.js
+++ Studenten/js/init.js
@@ -15,15 +15,22 @@
 
     // Plugin initialization
 	$('.more').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on click
-      alignment: 'left', // Aligns dropdown to left or right edge (works with constrain_width)
-      gutter: 0, // Spacing from edge
-      belowOrigin: false // Displays dropdown below the button
-    }
-  );
+		inDuration: 300,
+		outDuration: 225,
+		constrain_width: false, // Does not change width of dropdown to that of the activator
+		hover: false, // Activate on click
+		alignment: 'left', // Aligns dropdown to left or right edge (works with constrain_width)
+		gutter: 0, // Spacing from edge
+		belowOrigin: false // Displays dropdown below the button
+		}
+	);
+	
+	// Pikadate datepicker
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15 // Creates a dropdown of 15 years to control year
+	});
+	
 	$('.materialboxed').materialbox();
 	$("#studentsTable").tablesorter();
 	$("#usersTable").tablesorter(); 
